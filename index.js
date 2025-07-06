f8130482dc2fcdfa8b6caec71f011babfabc3273
tascobrar <167150161+tascobrar@users.noreply.github.com>
2025-07-06 10:34:08 -0400

    Add data files structure and simple trips reading

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log("Hello city!");
+const fs = require("fs");
+
+const TRIPS_DIRECTORY = "./data/trips";
+
+if (!fs.existsSync(TRIPS_DIRECTORY)) {
+    console.error(`${TRIPS_DIRECTORY} not found!`);
+}
+else {
+    fs.readdir("./data/trips", (error, files) => {
+        if (error) {
+            console.error("Couldn't read trips directory!", error);
+            return;
+        }
+        files.forEach((file) => {
+            console.log(`Found trip file ${file}`)
+        })
+    });
+}
