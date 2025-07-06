f2ad502cf24174e66a6212d0084e225c2a6c0003
tascobrar <167150161+tascobrar@users.noreply.github.com>
2025-07-06 15:10:37 -0400

    Put code in main function to allow for early returns instead of deeper if statements

--- index.js
+++ index.js
@@ -2,27 +2,29 @@ const fs = require("fs");
 
 const TRIPS_DIRECTORY = "./data/trips";
 
-if (!fs.existsSync(TRIPS_DIRECTORY)) {
-    console.error(`${TRIPS_DIRECTORY} not found!`);
-}
-else {
+function run() {
+    if (!fs.existsSync(TRIPS_DIRECTORY)) {
+        console.error(`${TRIPS_DIRECTORY} not found!`);
+        return;
+    }
     const tripFiles = fs.readdirSync(TRIPS_DIRECTORY);
     if (!tripFiles) {
         console.error("Couldn't read trips directory!");
+        return;
     }
-    else {
-        tripFiles.forEach((fileName) => {
-            console.log(`Found trip file ${fileName}`);
-            const tripFileContents = fs.readFileSync(`${TRIPS_DIRECTORY}/${fileName}`).toString();
-            const tripFileEntries = tripFileContents
-                .split("\n")
-                .slice(1)
-                .slice(0, -1)
-                .map(entry => entry.split(","))
-                .map((tripFileEntry) => {
-                    tripFileEntry[6] = tripFileEntry[6].replaceAll("\r", ""); 
-                    return tripFileEntry;
-                });
-        })
-    }
+    tripFiles.forEach((fileName) => {
+        console.log(`Found trip file ${fileName}`);
+        const tripFileContents = fs.readFileSync(`${TRIPS_DIRECTORY}/${fileName}`).toString();
+        const tripFileEntries = tripFileContents
+        .split("\n")
+        .slice(1)
+        .slice(0, -1)
+        .map(entry => entry.split(","))
+        .map((tripFileEntry) => {
+            tripFileEntry[6] = tripFileEntry[6].replaceAll("\r", ""); 
+            return tripFileEntry;
+        });
+    })
 }
+
+run();
