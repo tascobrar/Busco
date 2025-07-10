2f5e06f32c7de44d985e1909069020bbb55f2624
tascobrar <167150161+tascobrar@users.noreply.github.com>
2025-07-10 00:14:10 -0400

    Move files to src and run dir

--- src/index.js
+++ src/index.js
@@ -4,16 +4,16 @@ import { initializeShapes, testShapes } from "./shapes.js";
 import { initializeTrips, testTrips } from "./trips.js";
 
 function initialize() {
+    process.chdir("./run");
     return initializeTrips() 
         && initializeShapes() 
         && initializeRouteShapeDesignations() 
         && initializeShapeCoordinateDesignations();
 }
-
 initialize();
 
 testTrips("Q53+");
 testShapes("SBS440520");
 testRouteShapeDesignations("Q35");
 testShapeCoordinateDesignations("SBS440520");
-console.log(getRouteCoordinates("Q44+"));
+console.log(getRouteCoordinates("Q50"));
