cd5f24ef3ef483d95e585025ad3050646818739b
tascobrar <167150161+tascobrar@users.noreply.github.com>
2025-07-12 17:42:56 -0400

    Add express types and simple webserver

--- src/map/index.ts
+++ src/map/index.ts
@@ -1,12 +1,25 @@
-import { initialize } from "../main/initialization";
+import { InitializationResult, initialize } from "../main/initialization";
+
+import express, { Application } from "express"; 
+
+const PORT: number = 3000;
 
 function main() {
     process.chdir("./run");
-    let result = initialize();
+    let result: InitializationResult = initialize();
     if (!result.succeeded) {
         throw new Error(`Map initialization failed! ${result.message}`);
     }
-
+    const app: Application = express();
+    app.listen(PORT, "localhost", () => {
+        console.log(`Running map server on port ${PORT}`);
+    });
+    app.get('/', (req, res) => {
+        res.writeHead(200, "{Content-Type: text/html"); 
+        res.end(`
+            yo   
+        `);
+    });
 }
 
 main();
