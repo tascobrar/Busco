187d952c0d906448df07e87add8b4bfba41960de
tascobrar <167150161+tascobrar@users.noreply.github.com>
2025-07-11 22:13:49 -0400

    Migrate shapes to typescript

--- src/shapes.ts
+++ src/shapes.ts
@@ -1,26 +1,26 @@
 import fs from "fs";
-import { getFileContentsAsEntries } from "./utils";
+import { Entries, EntryBook, getFileContentsAsEntries } from "./utils";
 
-export const shapeFileToShapeEntries = new Map();
-export let allShapeEntries = [];
+export const shapeFileToShapeEntries: EntryBook = new Map<string, Entries>();
+export let allShapeEntries: Entries = [];
 
-const SHAPES_DIRECTORY = "./data/shapes";
+const SHAPES_DIRECTORY: string = "./data/shapes";
 
-export function initializeShapes() {
+export function initializeShapes(): boolean {
     if (!fs.existsSync(SHAPES_DIRECTORY)) {
         console.error(`${SHAPES_DIRECTORY} not found!`);
         return false;
     }
-    const shapeFiles = fs.readdirSync(SHAPES_DIRECTORY);
+    const shapeFiles: string[] = fs.readdirSync(SHAPES_DIRECTORY);
     if (!shapeFiles) {
         console.error("Couldn't read shapes directory!");
         return false;
     }
     shapeFiles.forEach((shapeFileName) => {
-        const shapeFileContents = fs.readFileSync(`${SHAPES_DIRECTORY}/${shapeFileName}`).toString();
-        const shapeEntries = getFileContentsAsEntries(shapeFileContents);
+        const shapeFileContents: string = fs.readFileSync(`${SHAPES_DIRECTORY}/${shapeFileName}`).toString();
+        const shapeEntries: Entries = getFileContentsAsEntries(shapeFileContents);
         shapeEntries.forEach((shapeEntry) => {
-            shapeEntry[3] = shapeEntry[3].replaceAll("\r", "");
+            shapeEntry[3] = shapeEntry[3].replace(/\r/g, "");
         });
         shapeFileToShapeEntries.set(shapeFileName, shapeEntries);
         allShapeEntries = allShapeEntries.concat(shapeEntries);
@@ -28,7 +28,7 @@ export function initializeShapes() {
     return true;
 }
 
-export function testShapes(shapeName) {
+export function testShapes(shapeName: string) {
     console.log(`Testing shapes with name ${shapeName}`);
     allShapeEntries.forEach((shapeEntry) => {
         if (shapeEntry[0] == shapeName) {
