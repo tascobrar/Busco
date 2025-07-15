6fe0ba16f230a729fdb4f886727cdbcee5a91dac
tascobrar <167150161+tascobrar@users.noreply.github.com>
2025-07-15 11:38:24 -0400

    Add simple templated js and tsx instead of compiling

--- src/host/index.ts
+++ src/host/index.ts
@@ -2,6 +2,7 @@ import { InitializationResult, initialize } from "../main/initialization";
 
 import express, { Application } from "express"; 
 import { allTripEntries, TRIP_ROUTE_INDEX } from "../main/trips";
+import { getRouteCoordinates } from "../main/shapeCoordinateDesignations";
 
 const PORT: number = 3000;
 
@@ -11,24 +12,21 @@ function main() {
     if (!result.succeeded) {
         throw new Error(`Map initialization failed! ${result.message}`);
     }
+    process.chdir(__dirname);
+    console.log(__dirname);
     const app: Application = express();
+    app.set("view engine", "ejs");
+    app.set("views", "./views");
+    app.get('/busmap', (req, res) => {
+        let bus = req.query["bus"];
+        const data = {
+            title: `Busco: ${bus} bus map`,
+            coordinateString: JSON.stringify(getRouteCoordinates(bus.toString()))
+        };
+        res.render("index", {data});
+    });
     app.listen(PORT, "localhost", () => {
         console.log(`Running map server on port ${PORT}`);
-    });
-    app.get('/busmap', (req, res) => {
-        res.writeHead(200, "{Content-Type: text/html"); 
-        let bus = req.query["bus"];
-        if (!bus) {
-            res.end(`
-                you did not select a bus
-            `);
-            return;
-        }
-        res.end(`
-            you selected the ${bus}
-            here's some information
-            ${allTripEntries.filter((tripEntry) => tripEntry[TRIP_ROUTE_INDEX] == bus)}
-        `)
     });
 }
 
