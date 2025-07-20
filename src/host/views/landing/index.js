ae1320dd0c2031600cfb092d3556edb4080ddf5f
tascobrar <167150161+tascobrar@users.noreply.github.com>
2025-07-20 19:52:31 -0400

    Make setting the bus polyline a general function

--- src/host/views/landing/index.js
+++ src/host/views/landing/index.js
@@ -12,14 +12,29 @@ const layer = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: attribution
 }).addTo(map);
 map.setZoom(zoom);
-const bus = "SIM30";
-fetch(`http://localhost:3000/rawroutecoords?bus=${bus}`).then((response) => response.json().then((listOfCoordinatePairs) => {
-    for (let i = 0; i < listOfCoordinatePairs.length; i ++) {
-        const coordinatePairs = listOfCoordinatePairs[i];
-        const polyline = L.polyline(coordinatePairs, {color: "blue"}).addTo(map);
-        if (i == 0) {
-            const startPopup = L.popup().setLatLng(L.latLng(coordinatePairs[0][0], coordinatePairs[0][1])).setContent(bus);
-            map.openPopup(startPopup);
-        }
+
+let displayedRouteElements;
+
+async function getRawRouteCoords(bus) {
+    return await fetch(`http://localhost:3000/rawroutecoords?bus=${bus}`).then((response) => response.json());
+}
+
+function setDisplayedRoute(bus) {
+    if (displayedRouteElements) {
+        map.removeLayer(displayedRouteElements);
     }
-}));
+    displayedRouteElements = L.layerGroup();
+    getRawRouteCoords(bus).then((listOfCoordinatePairs) => {
+        for (let i = 0; i < listOfCoordinatePairs.length; i ++) {
+            const coordinatePairs = listOfCoordinatePairs[i];
+            const polyline = L.polyline(coordinatePairs, {color: "blue"}).addTo(displayedRouteElements);
+        
+            if (i == 0) {
+                const startPopup = L.popup().setLatLng(L.latLng(coordinatePairs[0][0], coordinatePairs[0][1])).setContent(bus);
+                startPopup.addTo(displayedRouteElements);
+                map.openPopup(startPopup);
+            }
+        }
+    });
+    displayedRouteElements.addTo(map);
+}
